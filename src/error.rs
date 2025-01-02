fb846c36bb32085dcf6bd1cbb29bb4a4b36d8e64
Paulo Albuquerque <paulora2405@gmail.com>
2025-01-02 18:19:30 -0300

    Add new errors

--- src/error.rs
+++ src/error.rs
@@ -12,4 +12,11 @@ pub enum Error {
     Conversion(#[from] std::num::TryFromIntError),
     #[error(transparent)]
     Utf8(#[from] std::string::FromUtf8Error),
+    #[error("end of stream error")]
+    /// Attempting to extract a value failed due to the frame being fully consumed
+    EndOfStream,
+    #[error("unknown command `{0}`")]
+    UnknownCommand(String),
+    #[error("wrong frame type: {0}")]
+    WrongFrameType(String),
 }
