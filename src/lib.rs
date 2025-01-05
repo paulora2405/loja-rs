950ba8bc8480321a7bb424358d53f976e4d9d2bb
Paulo Albuquerque <paulora2405@gmail.com>
2025-01-04 21:21:42 -0300

    Replace `Db` type alias with actual `Db` implementation

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,17 @@ pub mod connection;
 pub mod db;
 pub mod error;
 pub mod frame;
+pub mod server;
 pub mod shutdown;
 
 pub use cmd::CommandVariant;
 pub use connection::Connection;
+pub(crate) use db::Db;
 pub use error::Error;
 pub use frame::Frame;
-pub use shutdown::Shutdown;
+pub(crate) use shutdown::Shutdown;
 
 pub const DEFAULT_PORT: u16 = 6379;
 pub const DEFAULT_HOST: &str = "0.0.0.0";
 
 type NVResult<T> = std::result::Result<T, crate::error::Error>;
-
-pub type Db = Arc<RwLock<HashMap<String, Bytes>>>;
