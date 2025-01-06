ffae49b7a3b8a15c452e9ce93d22b255223e9fa8
Paulo Albuquerque <paulora2405@gmail.com>
2025-01-06 09:07:38 -0300

    Fix example

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -1,8 +1,6 @@
-use mini_redis::{client, Result};
-
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() -> anyhow::Result<()> {
+    let mut client = loja::Client::connect("127.0.0.1:6379").await?;
 
     client.set("hello", "world".into()).await?;
 
