3347d3d8852930759534e1da97a5f2ae412447c7
Paulo Albuquerque <paulora2405@gmail.com>
2025-01-08 16:43:22 -0300

    Add cli to server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use clap::{command, Parser};
 use loja::{server, DEFAULT_HOST, DEFAULT_PORT};
 use tokio::net::TcpListener;
 use tracing::info;
@@ -6,14 +7,30 @@ use tracing::info;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
-    let addr = format!("{DEFAULT_HOST}:{DEFAULT_PORT}");
+    let cli = LojaServerCli::parse();
+    let addr = std::net::SocketAddr::new(cli.host, cli.port);
 
     let listener = TcpListener::bind(&addr)
         .await
         .context("failed to bind tcp listener")?;
-    info!("Listening on {addr}");
+    info!("listening on {addr}");
 
     server::run(listener, tokio::signal::ctrl_c()).await;
 
     Ok(())
+}
+
+#[derive(Debug, Parser)]
+#[command(name = "loja-server", version, author, disable_help_flag(true))]
+/// A simple Redis cli client
+struct LojaServerCli {
+    #[clap(long, action = clap::ArgAction::HelpLong)]
+    /// Display cli help.
+    help: Option<bool>,
+    #[arg(short, long, default_value = DEFAULT_HOST)]
+    /// Host to bind to.
+    host: std::net::IpAddr,
+    #[arg(short, long, default_value_t = DEFAULT_PORT)]
+    /// Port to bind to.
+    port: u16,
 }
