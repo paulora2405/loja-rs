6ffe9cfca50bb97dd1533206e2e601f058dee13b
Paulo Albuquerque <paulora2405@gmail.com>
2025-01-09 23:05:28 -0300

    Fix example

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -1,12 +1,14 @@
+use loja::Client;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut client = loja::Client::connect("127.0.0.1:6379").await?;
+    let mut client = Client::connect("127.0.0.1:6379").await?;
 
     client.set("hello", "world".into()).await?;
 
     let result = client.get("hello").await?;
 
-    println!("got this shit from server: result={result:?}");
+    println!("got this from server: result={result:?}");
 
     Ok(())
 }
